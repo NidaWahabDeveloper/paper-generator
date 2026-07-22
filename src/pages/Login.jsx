b339f246f42c136df45aaef639a87d2b5dce31a9
Nida Wahab <savepakistan1486@gmail.com>
2026-07-22 11:44:09 +0500

    Add password show/hide toggle on Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
+import {useState} from "react";
 
 // Ye sirf UI hai — abhi real login/authentication nahi hai
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="min-h-screen flex items-center justify-center bg-bgsoft">
       <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-8 w-full max-w-sm">
@@ -16,8 +18,14 @@ export default function Login() {
         <input className="input mb-4" placeholder="teacher@school.com" />
 
         <label className="block text-xs font-semibold text-gray-500 mb-1.5">Password</label>
-        <input type="password" className="input mb-6" placeholder="••••••••" />
 
+        {/* <input type="password" className="input mb-6" placeholder="••••••••" /> */}
+
+        {/* <input type={showPassword ? "text" : "password"} className="input mb-6" placeholder="••••••••" /> */}
+<div className="relative mb-6">
+<input type={showPassword ? "text" : "password"} className="input" placeholder=".........." />
+<button type="button" onClick={()=> setShowPassword(!showPassword)} className="absolute right-3 top-1/2 -translate-y-1/2 "> {showPassword ? "🙈" : "👁"} </button>
+</div>
         <Link to="/">
           <Button type="button">Log In</Button>
         </Link>
